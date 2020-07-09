edd3e4ace38489334351b7867904d114c6d4d945
thanthanmyint433 <thanthanmyint433@gmail.com>
2020-07-09 13:47:24 +0630

    modify:implement interface for service

---
+++ pet-clinic-data/src/main/java/com/example/demo/services/VetService.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import com.example.demo.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
