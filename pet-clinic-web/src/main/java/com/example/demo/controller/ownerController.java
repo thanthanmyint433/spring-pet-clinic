e73851c2647d8bbbff77b1948f94510b400b25be
thanthanmyint433 <thanthanmyint433@gmail.com>
2020-07-10 11:02:03 +0630

    add:owner controller and index page

--- pet-clinic-web/src/main/java/com/example/demo/controller/ownerController.java
+++ pet-clinic-web/src/main/java/com/example/demo/controller/ownerController.java
@@ -2,10 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping("/owners")
 @Controller
 public class ownerController {
-    @RequestMapping({"/owners","/owners/index","/owners/index.html"})
+    @RequestMapping({"","/index","/index.html"})
     public String ListOwner(){
         return "owners/index";
     }
