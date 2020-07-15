b1f4404b7fbb5d544febbf3b7a03769d41ff7ce8
thanthanmyint433 <thanthanmyint433@gmail.com>
2020-07-15 14:13:04 +0630

    add: visit,update owner with relationship

--- pet-clinic-data/src/main/java/com/example/demo/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/Owner.java
@@ -1,4 +1,15 @@
 package com.example.demo.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Pet> petSet;
+
+    public Set<Pet> getPetSet() {
+        return petSet;
+    }
+
+    public void setPetSet(Set<Pet> petSet) {
+        this.petSet = petSet;
+    }
 }
