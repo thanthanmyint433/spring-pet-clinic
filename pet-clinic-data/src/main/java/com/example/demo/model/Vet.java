d4fdabd2b602a02ffe560797755f08a3826fcc8c
thanthanmyint433 <thanthanmyint433@gmail.com>
2020-07-21 13:03:18 +0630

    add: Convert vet to jpa properties #33

--- pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
@@ -1,9 +1,13 @@
 package com.example.demo.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany
+    @JoinTable(name = "vet_specialities",joinColumns = @JoinColumn(name = "vet_id"),inverseJoinColumns = @JoinColumn(name = "speciality"))
     private Set<Speciality> specialitySet=new HashSet<>();
 
     public Set<Speciality> getSpecialitySet() {
