2a573d0e01a90b835608589a1b511b307e9108cd
thanthanmyint433 <thanthanmyint433@gmail.com>
2020-07-21 11:41:38 +0630

    add: Convert owner to jpa properties #32

--- pet-clinic-data/src/main/java/com/example/demo/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/Pet.java
@@ -1,9 +1,22 @@
 package com.example.demo.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pet")
 public class Pet extends BaseEntity{
+    @Column(name = "pet_name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- pet-clinic-data/src/main/java/com/example/demo/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/PetType.java
@@ -1,6 +1,17 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_type")
 public class PetType extends BaseEntity {
+/*    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;*/
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
